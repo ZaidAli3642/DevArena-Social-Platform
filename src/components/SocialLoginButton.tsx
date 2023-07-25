4315893e0d07425b80cc76ca67acff10ed21a40d
Zaid Saleem <zaidali36422@gmail.com>
2023-07-26 00:39:14 +0500

    alt added

--- src/components/SocialLoginButton.tsx
+++ src/components/SocialLoginButton.tsx
@@ -8,7 +8,9 @@ interface Props {
 const SocialAuthButton: React.FC<Props> = ({ socialIcon, title }) => {
   return (
     <div className="flex justify-center items-center border-2 border-light-grey rounded-full px-20 py-2 my-4 cursor-pointer">
-      {socialIcon && <Image src={socialIcon} className="w-7 me-2" />}
+      {socialIcon && (
+        <Image src={socialIcon} alt="social-icon" className="w-7 me-2" />
+      )}
       {title && (
         <button className="border-0 outline-none text-sm">{title}</button>
       )}
